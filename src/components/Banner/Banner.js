626c59913182199345f74ba422e479fae848df11
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-04-30 12:11:10 +0600

    banner section added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+    <header>
+      <div className='banner-container'>
+        <h1>A Fruit Inventory Management System</h1>
+        <h3>A system that makes your fruit inventory management easier</h3>
+        <a href='#inventory'>GO TO INVENTORY</a>
+      </div>
+    </header>
+  )
+}
+
+export default Banner
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Banner from '../../components/Banner/Banner'
 import './Home.css'
 
 const Home = () => {
   return (
     <div>
-      <h1>This is home page</h1>
+      <Banner />
     </div>
   )
 }
