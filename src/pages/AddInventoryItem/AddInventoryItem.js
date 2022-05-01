cda69821c0747c9c9fef8bfeab51045890eb7eb2
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-01 20:43:21 +0600

    add inventory item api implemented

--- src/pages/AddInventoryItem/AddInventoryItem.js
+++ src/pages/AddInventoryItem/AddInventoryItem.js
@@ -1,42 +1,92 @@
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 import TitleUnderline from '../../components/TitleUnderline/TitleUnderline'
 import './AddInventoryItem.css'
 
 const AddInventoryItem = () => {
+  const { register, handleSubmit } = useForm()
+  const addInventoryItemOnSubmit = (fruitData, event) => {
+    fetch('http://localhost:5000/fruits', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(fruitData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        event.target.reset()
+      })
+  }
   return (
     <div className='container'>
       <h2 className='text-center mt-3'>Add Inventory Item</h2>
       <TitleUnderline />
       <div>
-        <Form className='w-50 mx-auto border p-3 rounded shadow mb-3'>
+        <Form
+          className='w-50 mx-auto border p-3 rounded shadow mb-3'
+          onSubmit={handleSubmit((data, event) =>
+            addInventoryItemOnSubmit(data, event)
+          )}
+        >
           <Form.Group className='mb-3' controlId='formBasicName'>
             <Form.Label>Item Name</Form.Label>
-            <Form.Control type='text' name='name' placeholder='Enter Name' />
+            <Form.Control
+              type='text'
+              placeholder='Enter Name'
+              {...register('name')}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicPrice'>
             <Form.Label>Price</Form.Label>
-            <Form.Control type='number' placeholder='Price' />
+            <Form.Control
+              type='number'
+              placeholder='Price'
+              {...register('price')}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicQuantity'>
             <Form.Label>Quantity</Form.Label>
-            <Form.Control type='number' placeholder='Quantity' />
+            <Form.Control
+              type='number'
+              placeholder='Quantity'
+              {...register('quantity')}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicSupplierName'>
             <Form.Label>Supplier Name</Form.Label>
-            <Form.Control type='text' placeholder='Supplier Name' />
+            <Form.Control
+              type='text'
+              placeholder='Supplier Name'
+              {...register('supplier')}
+            />
           </Form.Group>
-          <Form.Group className='mb-3' controlId='formBasicSupplierEmail'>
-            <Form.Label>Supplier Email</Form.Label>
-            <Form.Control type='email' placeholder='Supplier Email' />
+          <Form.Group className='mb-3' controlId='formBasicEmail'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Email'
+              {...register('email')}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicImageURL'>
             <Form.Label>Image URL</Form.Label>
-            <Form.Control type='text' placeholder='Image URL' />
+            <Form.Control
+              type='text'
+              placeholder='Image URL'
+              {...register('image')}
+            />
           </Form.Group>
           <Form.Group className='mb-3' controlId='formBasicPrice'>
             <Form.Label>Description</Form.Label>
-            <Form.Control as='textarea' rows='3' placeholder='Description' />
+            <Form.Control
+              as='textarea'
+              rows='3'
+              placeholder='Description'
+              {...register('desc')}
+            />
           </Form.Group>
           <div className='text-center'>
             <Button variant='primary' type='submit'>
