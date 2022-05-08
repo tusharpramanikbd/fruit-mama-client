1c9901b892cbe26c3ce13692f423d2daed6112de
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-08 13:24:50 +0600

    not found page design modified

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='container not-found-container'>
       <PageTitle title='Not Found' />
       <h1>This is not found page</h1>
     </div>
